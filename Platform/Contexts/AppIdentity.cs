d47ba05c00ac9e677b3d367197ac2b8105760fb4
a.nasserr1 <a.nasserr1@outlook.com>
2024-02-04 22:07:21 +0200

    Create Identity Tables

---
+++ Platform/Contexts/AppIdentity.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Platform.Contexts
+{
+    public class AppIdentity : IdentityDbContext<IdentityUser,IdentityRole,string>
+    {
+        public AppIdentity(DbContextOptions<AppIdentity> options) : base(options) { }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+
+            }
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
